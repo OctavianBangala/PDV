9564a8fa8c1e1bbcf01b68081b818b961d512b7a
OctavianBangala <71028367+OctavianBangala@users.noreply.github.com>
2026-08-04 11:04:14 +0300

    Create rooms.js

---
+++ js/rooms.js
@@ -0,0 +1,113 @@
+// ========================================
+// PENSIUNEA PĂDUREA VERDE
+// rooms.js
+// ========================================
+
+const ROOMS = [
+
+    {
+        id: "1A",
+        corp: "A",
+        name: "Camera Dublă Deluxe",
+        capacity: 2
+    },
+
+    {
+        id: "2A",
+        corp: "A",
+        name: "Camera Dublă Deluxe",
+        capacity: 2
+    },
+
+    {
+        id: "3A",
+        corp: "A",
+        name: "Studio Deluxe",
+        capacity: 3
+    },
+
+    {
+        id: "4A",
+        corp: "A",
+        name: "Apartament 2 Camere",
+        capacity: 4
+    },
+
+    {
+        id: "5A",
+        corp: "A",
+        name: "Camera Triplă Superioară",
+        capacity: 3
+    },
+
+    {
+        id: "6A",
+        corp: "A",
+        name: "Camera Dublă Economy",
+        capacity: 2
+    },
+
+    {
+        id: "1B",
+        corp: "B",
+        name: "Camera Dublă Superioară",
+        capacity: 2
+    },
+
+    {
+        id: "2B",
+        corp: "B",
+        name: "Camera Triplă Superioară",
+        capacity: 3
+    },
+
+    {
+        id: "3B",
+        corp: "B",
+        name: "Camera Dublă Superioară",
+        capacity: 2
+    },
+
+    {
+        id: "4B",
+        corp: "B",
+        name: "Camera Dublă",
+        capacity: 2
+    },
+
+    {
+        id: "5B",
+        corp: "B",
+        name: "Camera Triplă Superioară",
+        capacity: 3
+    },
+
+    {
+        id: "6B",
+        corp: "B",
+        name: "Camera Dublă Superioară",
+        capacity: 2
+    },
+
+    {
+        id: "7B",
+        corp: "B",
+        name: "Apartament 2 Camere",
+        capacity: 4
+    },
+
+    {
+        id: "8B",
+        corp: "B",
+        name: "Camera Triplă Superioară",
+        capacity: 3
+    },
+
+    {
+        id: "9B",
+        corp: "B",
+        name: "Camera Dublă",
+        capacity: 2
+    }
+
+];
