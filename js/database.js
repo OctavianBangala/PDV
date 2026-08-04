7a1ac2330f3b3949f51cb9e4721acaaa9e56aae5
OctavianBangala <71028367+OctavianBangala@users.noreply.github.com>
2026-08-04 10:25:38 +0300

    Create database.js

---
+++ js/database.js
@@ -0,0 +1,49 @@
+// ======================================
+// PENSIUNEA PĂDUREA VERDE
+// database.js
+// ======================================
+
+const DB_NAME = "PDV_DATABASE";
+const DB_VERSION = 1;
+
+let db = null;
+
+function initDatabase() {
+
+    const request = indexedDB.open(DB_NAME, DB_VERSION);
+
+    request.onerror = () => {
+
+        console.error("Eroare deschidere baza de date.");
+
+    };
+
+    request.onsuccess = (event) => {
+
+        db = event.target.result;
+
+        console.log("Baza de date conectată.");
+
+    };
+
+    request.onupgradeneeded = (event) => {
+
+        db = event.target.result;
+
+        if (!db.objectStoreNames.contains("reservations")) {
+
+            const reservations = db.createObjectStore("reservations", {
+                keyPath: "id",
+                autoIncrement: true
+            });
+
+            reservations.createIndex("client", "client");
+            reservations.createIndex("phone", "phone");
+            reservations.createIndex("checkIn", "checkIn");
+            reservations.createIndex("checkOut", "checkOut");
+
+        }
+
+    };
+
+}
