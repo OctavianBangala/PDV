9992dadcb43e6d9ee045c0d8d281ec9f95798528
OctavianBangala <71028367+OctavianBangala@users.noreply.github.com>
2026-08-04 10:23:06 +0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// ==========================================
+// PENSIUNEA PADUREA VERDE
+// app.js
+// ==========================================
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Aplicatia a pornit.");
+
+});
